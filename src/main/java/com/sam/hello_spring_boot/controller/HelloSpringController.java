9a8c109fb32df7514184efab84b73b4b1f0f47ed
samro123 <thsam.20it5@vku.udn.vn>
2025-01-18 14:35:56 +0700

    Hello

--- src/main/java/com/sam/hello_spring_boot/controller/HelloSpringController.java
+++ src/main/java/com/sam/hello_spring_boot/controller/HelloSpringController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloSpringController {
     @GetMapping("/hello")
     String sayHello(){
-        return "Hello Sam";
+        return "Hello Ro";
     }
 
 }
